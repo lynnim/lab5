c3ba0add134b27b06564507c6936225e0a0bf315
Lynn Im <yli2101@barnard.edu>
2018-12-13 13:50:14 -0500

    Update part3.py

--- part3.py
+++ part3.py
@@ -58,7 +58,6 @@ class Follower:
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
  
     if Y['m00'] > 0 and not self.STOP:
-
         if R['m00'] > 0 and not self.STOP: 
             res_arrowL = cv2.matchTemplate(img_gray,gray_left,cv2.TM_CCOEFF_NORMED)
             res_arrowR = cv2.matchTemplate(img_gray,gray_right,cv2.TM_CCOEFF_NORMED)
@@ -79,6 +78,7 @@ class Follower:
                 self.cmd_vel_pub.publish(self.twist)
                 self.twist.linear.x = .45
                 self.twist.angular.z = 0.3
+            
             elif min_valR < -0.81 and min_valR < min_valL and min_valS > -0.05:
                 #print("Turning right")
                 self.twist.linear.x = .45
@@ -87,6 +87,7 @@ class Follower:
                 self.twist.linear.x = .45
                 self.twist.angular.z = -0.2
                 self.cmd_vel_pub.publish(self.twist)
+            
             elif min_valS < -0.05 and min_valL < -0.81 and min_valR < -0.83:
                 #print("Stopping")
                 self.stop = True 
@@ -98,6 +99,7 @@ class Follower:
                 self.twist.linear.x = 1
                 self.twist.angular.z = 0
                 self.cmd_vel_pub.publish(self.twist)
+            
             else:
                 self.twist.linear.x = 0.3
                 self.cmd_vel_pub.publish(self.twist)
@@ -106,7 +108,7 @@ class Follower:
             if not self.STOP:
                 cx = int(Y['m10'] / Y['m00'])
                 cy = int(Y['m01'] / Y['m00'])
-                cv2.circle(image, (cx, cy), 20, (2, 166, 249), -1)
+                cv2.circle(image, (cx, cy), 6, (0,0,255), -1)
                 err = cx - w / 2
                 self.twist.linear.x = 0.2
                 self.twist.angular.z = -float(err) / 100
