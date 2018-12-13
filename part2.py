b80a337ed006605789e9f926931290b2b1a580ed
Lynn Im <yli2101@barnard.edu>
2018-12-13 18:50:03 -0500

    Update part2.py

--- part2.py
+++ part2.py
@@ -3,6 +3,18 @@ import rospy, cv2, cv_bridge, numpy
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 
+arrowL = cv2.imread('left1.png')
+arrowR = cv2.imread('right1.png')
+star = cv2.imread('star1.png')
+
+template_arrowL = cv2.resize(arrowL, (0,0), fx=0.3, fy=0.3) 
+template_arrowR = cv2.resize(arrowR, (0,0), fx=0.3, fy=0.3) 
+template_star = cv2.resize(star, (0,0), fx=0.3, fy=0.3) 
+
+gray_left = cv2.cvtColor(template_arrowL,cv2.COLOR_BGR2GRAY)
+gray_right = cv2.cvtColor(template_arrowR,cv2.COLOR_BGR2GRAY)
+gray_star = cv2.cvtColor(template_star,cv2.COLOR_BGR2GRAY)
+
 class Follower:
   def __init__(self):
     self.bridge = cv_bridge.CvBridge()
@@ -10,7 +22,7 @@ class Follower:
     self.image_sub = rospy.Subscriber('camera/rgb/image_raw', Image, self.image_callback)
     self.cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=1)
     self.twist = Twist()
-    self.stop = False
+    self.STOP = False
 
   def image_callback(self, msg):
     image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
@@ -26,22 +38,14 @@ class Follower:
     upper_red = numpy.array([10, 255, 255])
     mask_red = cv2.inRange(hsv, lower_red, upper_red)
 
-    lower_green = numpy.array([50, 100, 100])
-    upper_green = numpy.array([70, 255, 255])
-    mask_green = cv2.inRange(hsv, lower_green, upper_green)
-
-    lower_blue = numpy.array([110, 100, 100])
-    upper_blue = numpy.array([130, 255, 255])
-    mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
-
-    search_top = 5 * h / 6 
-    search_bot = search_top + 20 
+    search_top = 5 * h / 6  
+    search_bot = search_top + 20  
     mid = w/2
     mask_yellow[0:search_top, 0:w] = 0
     mask_yellow[search_bot:h, 0:w] = 0
     Y = cv2.moments(mask_yellow)
 
-    top_red = (6*h/7)+6
+    top_red = (6 * h / 7)+6
     bot_red = top_red + 20
     mid = w / 2
     mask_red[0:top_red, 0:w] = 0
@@ -50,66 +54,72 @@ class Follower:
     mask_red[0:h, mid+10:w] = 0
     R = cv2.moments(mask_red)
 
-    top_green = 4 * h / 5
-    bot_green = top_green + 30
-    mask_green[0:top_green, 0:w] = 0
-    mask_green[bot_green:h, 0:w] = 0
-    G = cv2.moments(mask_green)
-
-    top_blue = 4 * h / 5
-    bot_blue = top_blue + 30
-    mask_blue[0:top_blue, 0:w] = 0
-    mask_blue[bot_blue:h, 0:w] = 0
-    B = cv2.moments(mask_blue)
-
+    img = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+    #if Y['m00'] > 0 and not self.STOP:
     if Y['m00'] > 0:
-      if G['m00'] > 0:
-        cx_green = int(G['m10'] / G['m00'])
-        cy_green = int(G['m01'] / G['m00'])
-        cv2.circle(image, (cx_green, cy_green), 6, (0, 0, 255), -1)
-        #print("LEFT")
-        self.twist.linear.x = .45
-        self.twist.angular.z = 0.2
-        self.cmd_vel_pub.publish(self.twist)
+        if R['m00'] > 0: 
+            res_arrowL = cv2.matchTemplate(img_gray,gray_left,cv2.TM_CCOEFF_NORMED)
+            res_arrowR = cv2.matchTemplate(img_gray,gray_right,cv2.TM_CCOEFF_NORMED)
+            res_star = cv2.matchTemplate(img_gray,gray_star,cv2.TM_CCOEFF_NORMED)
+    
+            min_valL, max_valL, min_loc, max_loc = cv2.minMaxLoc(res_arrowL)
+            min_valR, max_valR, min_loc, max_loc = cv2.minMaxLoc(res_arrowR)
+            min_valS, max_valS, min_loc, max_loc = cv2.minMaxLoc(res_star)
 
-      elif B['m00'] > 0:
-        cx_blue = int(B['m10'] / B['m00'])
-        cy_blue = int(B['m01'] / B['m00'])
-        cv2.circle(image, (cx_blue, cy_blue), 6, (0, 0, 255), -1)
-        #print("RIGHT")
-        self.twist.linear.x = .45
-        self.twist.angular.z = -0.2
-        self.cmd_vel_pub.publish(self.twist)
+            print("min_valL: ", min_valL)
+            print("min_valR: ", min_valR)
+            print("min_valS: ", min_valS)
 
-      elif R['m00'] > 0:
-          cx_red = int(R['m10'] / R['m00'])
-          cy_red = int(R['m01'] / R['m00'])
-          cv2.circle(image, (cx_red, cy_red), 6, (0, 0, 225), -1)
-          err = cx_red - w / 2
-          #print("STOP")
-          self.twist.linear.x = 0.5
-          self.twist.angular.z = -float(err) / 100
-          self.cmd_vel_pub.publish(self.twist)
-          self.stop = True
+            # if min_valL < -0.81 and min_valL < min_valR and min_valS > -0.05: 
+            #     print("LEFT")
+            #     self.twist.linear.x = .45
+            #     self.twist.angular.z = 0.3
+            #     self.cmd_vel_pub.publish(self.twist)
+            #     self.twist.linear.x = .45
+            #     self.twist.angular.z = 0.3
+            
+            # elif min_valR < -0.81 and min_valR < min_valL and min_valS > -0.05:
+            #     print("RIGHT")
+            #     self.twist.linear.x = .45
+            #     self.twist.angular.z = -0.2
+            #     self.cmd_vel_pub.publish(self.twist)
+            #     self.twist.linear.x = .45
+            #     self.twist.angular.z = -0.2
+            #     self.cmd_vel_pub.publish(self.twist)
+            
+            # elif min_valS < -0.05 and min_valL < -0.81 and min_valR < -0.83:
+            #     print("STOP")
+            #     self.stop = True 
+            #     for i in range(0,150):
+            #       print(i)
+            #       self.twist.linear.x = 1
+            #       self.twist.angular.z = -1
+            #       self.cmd_vel_pub.publish(self.twist)
+            #     self.twist.linear.x = 1
+            #     self.twist.angular.z = 0
+            #     self.cmd_vel_pub.publish(self.twist)
+            
+            # else:
+            #     self.twist.linear.x = 0.3
+            #     self.cmd_vel_pub.publish(self.twist)
 
-      else:
-          if self.stop:
-              for i in range(0,150):
-                  self.twist.linear.x = 3
-                  self.twist.angular.z = -1
-                  self.cmd_vel_pub.publish(self.twist)
+        else:
+            # if not self.STOP:
+            cx = int(Y['m10'] / Y['m00'])
+            cy = int(Y['m01'] / Y['m00'])
+            cv2.circle(image, (cx, cy), 6, (0,0,255), -1)
+            err = cx - w / 2
+            self.twist.linear.x = 0.2
+            self.twist.angular.z = -float(err) / 100
+            self.cmd_vel_pub.publish(self.twist)
+            # else:
+            #     print("MOVING FORWARD")
+            #     self.twist.linear.x = 0
+            #     self.twist.angular.z = 0
+            #     self.cmd_vel_pub.publish(self.twist)
 
-              self.twist.linear.x = 0
-              self.twist.angular.z = 0
-              self.cmd_vel_pub.publish(self.twist)
-          else:
-              cx = int(Y['m10'] / Y['m00'])
-              cy = int(Y['m01'] / Y['m00'])
-              cv2.circle(image, (cx, cy), 6, (0, 0, 225), -1)
-              err = cx - w / 2
-              self.twist.linear.x = 0.55
-              self.twist.angular.z = -float(err) / 100
-              self.cmd_vel_pub.publish(self.twist)
     cv2.imshow("window", image)
     cv2.waitKey(3)
 
