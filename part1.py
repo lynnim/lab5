033e92ed6ed46d14620b9b0d2920bae44e8012f7
Lynn Im <yli2101@barnard.edu>
2018-12-13 12:22:50 -0500

    Update part1.py

--- part1.py
+++ part1.py
@@ -34,7 +34,9 @@ class Follower:
       self.twist.linear.x = 0.2
       self.twist.angular.z = -float(err) / 100
       self.cmd_vel_pub.publish(self.twist)
-    cv2.imshow("window", image)
+    cv.namedWindow("window", image)
+    cv2.resizeWindow("window", 600, 600)
+    #cv2.imshow("window", image)
     cv2.waitKey(3)
 
 rospy.init_node('follower')
