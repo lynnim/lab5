e78bd79af703b63462f8e0d5c49226aa8f422112
Lynn Im <yli2101@barnard.edu>
2018-12-13 22:10:59 -0500

    Update part3.py

--- part3.py
+++ part3.py
@@ -109,7 +109,7 @@ class Follower:
                 #print("follow yellow")
                 cx = int(Y['m10'] / Y['m00'])
                 cy = int(Y['m01'] / Y['m00'])
-                cv2.circle(image, (cx, cy), 20, (2, 166, 249), -1)
+                cv2.circle(image, (cx, cy), 6, (0, 0, 255), -1)
                 err = cx - w / 2
                 self.twist.linear.x = 0.2
                 self.twist.angular.z = -float(err) / 100
